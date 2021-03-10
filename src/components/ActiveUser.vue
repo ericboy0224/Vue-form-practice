f53d63ad737b6480e89c54d3ec1e102b98e1ed0f
Jia-Hao Lin <ericboy0224@gmail.com>
2021-03-10 10:30:59 +0800

    Create ActiveUser.vue
    It's a component shows a form to get data from other components .

---
+++ src/components/ActiveUser.vue
@@ -0,0 +1,19 @@
+<template>
+  <ul>
+      <li>
+          <h2>
+              Name: {{ name }}
+          </h2>
+          <h3>
+              Age: {{ age }}
+          </h3>
+      </li>
+  </ul>
+</template>
+
+<script>
+export default {
+    props:['id','name','age']
+
+}
+</script>
